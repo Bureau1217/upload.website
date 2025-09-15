7f26be268713d6f5275edd25d6333fcfed19b65d
gabrielabergel <44164440+gabrielabergel@users.noreply.github.com>
2025-09-15 10:25:29 +0200

    Update params.ts

--- composables/params.ts
+++ composables/params.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8000'
-// export const API_BASE_URL = 'url_prod'
+// export const API_BASE_URL = 'http://localhost:8000'
+export const API_BASE_URL = 'https://cms.upload-biennale.ch'
